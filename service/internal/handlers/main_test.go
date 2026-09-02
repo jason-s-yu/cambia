9d7116b7eb1c6e706a5f9bc2a22fd0e20ef60e82
jason-s-yu <jyu031103@gmail.com>
2026-09-01 22:42:57 -0700

    Drop the pgxpool import the handlers test main no longer uses (cambia-1830)

--- service/internal/handlers/main_test.go
+++ service/internal/handlers/main_test.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/jason-s-yu/cambia/service/internal/database"
 	"github.com/jason-s-yu/cambia/service/internal/testutil"
